28988b54c67bfedbcb625b8dc49441d4615498ce
Zul_Dev <felipe.jimenez.bahamondes@estudiante.ipss.cl>
2026-07-22 20:01:07 -0400

    Inicio: maquetado de cards

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -16,7 +16,41 @@
             <h1 class="text-4xl md:text-6xl font-bold mb-4">Techsolutions</h1>
             <p class="text-lg md:text-xl max-w-2x1">Ofrecemos cobertura para la zona sur del pais</p>
         </div>
-        
+
+    </div>
+
+    {{-- Contenido texto & cards --}}
+    <div class="content-container mt-16 md:mt-24 text-center">
+        <h3 class="font-bold text-3xl">Nuestros servicios</h3>
+        <p class="mt-4">En Techsolutions ofrecemos una gama de servicios enfocados en el area informatica.</p>
+    </div>
+
+    <div class="mt-4 grid grid-cols-1 md:grid-cols-4 gap-6 text-center">
+        {{-- cards --}}
+
+        {{-- card 1 --}}
+        <div class="border rounded-lg p-6 shadow-sm">
+            <h4 class="font-bold text-xl mb-2"></h4>
+            <p>soy un servicio ~ camaras</p>
+        </div>
+
+        {{-- card 2 --}}
+        <div class="border rounded-lg p-6 shadow-sm">
+            <h4 class="font-bold text-xl mb-2"></h4>
+            <p>soy un servicio</p>
+        </div>
+
+        {{-- card 3 --}}
+        <div class="border rounded-lg p-6 shadow-sm">
+            <h4 class="font-bold text-xl mb-2"></h4>
+            <p>soy un servicio</p>
+        </div>
+
+        {{-- card 4 --}}
+        <div class="border rounded-lg p-6 shadow-sm">
+            <h4 class="font-bold text-xl mb-2"></h4>
+            <p>soy un servicio</p>
+        </div>
     </div>
 
 </x-layout>
